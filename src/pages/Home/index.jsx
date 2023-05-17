540d5831bf18e42436d609db6361dcc9b6353dce
pedro558 <pedroafonso109@gmail.com>
2023-05-17 10:30:20 -0300

    :sparkles: feat: title and newMovie button added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,20 @@
-import { Container, Title, Content } from "./styles";
+import { Container, Title, NewMovie, Content } from "./styles";
 
 import { Header } from "../../components/Header";
+import { FiPlus } from 'react-icons/fi'
 
 export function Home() {
   return (
     <Container>
       <Header />
-      {/*
-      <Title></Title>
-      <Content></Content>
-      */
-      }
+      
+      <Title>
+        <h1>Meus filmes</h1>
+        <NewMovie>
+          <FiPlus/>
+          Adicionar filme
+        </NewMovie>
+      </Title>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -17,10 +17,30 @@ export const Container = styled.div`
 
 export const Title = styled.div`
   grid-area: title;
-  background-color: blue;
-  width: 70%;
-  height: 90px;
-  margin: 0 auto;
+  display: flex;
+  margin: 45px 120px 35px;
+  height: 50px;
+  align-items: center;
+  justify-content: space-between;
+
+  >h1{
+    font-size: 32px;
+  }
+`
+
+export const NewMovie = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 207px;
+  height: 48px;
+  border-radius: 8px;
+  font-size: 16px;
+  color: ${({theme}) => theme.COLORS.BACKGROUND_900};
+  background-color: ${({theme}) => theme.COLORS.PINK};
+  line-height: 21px;
+  gap: 8px;
+  padding: 14px 25px;
 `
 
 export const Content = styled.div`
