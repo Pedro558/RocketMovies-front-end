66e29d1702564468a9fdc8f72628845b26d925d6
pedro558 <pedroafonso109@gmail.com>
2023-05-18 18:29:23 -0300

    :lipstick: style: tag component stylized

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,12 +3,14 @@ export default {
     BACKGROUND_900: "#1C1B1E",
     BACKGROUND_800: "#262529",
     BACKGROUND_700: "#3E3B47",
+    BACKGROUND_600: "#312E38",
 
     PINK: "#FF859B",
     WHITE: "#F4EDE8",
     BLACK: "#0D0C0F",
 
     GRAY_100: "#999591",
+    GRAY_200: "#E5E5E5",
     GRAY_300: "#948F99",
 
 
