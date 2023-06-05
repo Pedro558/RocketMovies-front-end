80584160ffbaf0fdb15f4e5c69468a6990a88b91
pedro558 <pedroafonso109@gmail.com>
2023-06-05 17:10:09 -0300

    :lipstick: style: profile avatar and form

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -9,14 +9,52 @@ import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 export function Profile(){
   return(
     <Container>
-    <header>
-      <Link to='/'>
-        <FiArrowLeft />
-        <span>Voltar</span>
-      </Link>
-    </header>
+      <header>
+        <Link to='/'>
+          <FiArrowLeft />
+          <span>Voltar</span>
+        </Link>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img src="https://github.com/Pedro558.png" alt="Foto de perfil do pedro"/>
+
+          <label htmlFor='avatar'>
+            <FiCamera/>
+            <input type='file' id='avatar'/>
+          </label>
+
+        </Avatar>
+
+        <Input
+          placeholder='Nome'
+          type='text'
+          icon={FiUser}
+        />
+
+        <Input
+          placeholder='E-mail'
+          type='email'
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder='Senha Atual'
+          type='password'
+          icon={FiLock}
+        />
+
+        <Input
+          placeholder='Nova Senha'
+          type='password'
+          icon={FiLock}
+        />
+
+        <Button title='Salvar'/>
+      </Form>
+    </Container>
 
     
-  </Container>
   )
 }
