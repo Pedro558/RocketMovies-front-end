0e19e951269cdde2f9e433359afca9476e5e558f
pedro558 <pedroafonso109@gmail.com>
2023-06-15 17:21:29 -0300

    :construction: New Page style and structure

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,22 +1,36 @@
+import { Header } from '../../components/Header'
+import { Title } from '../../components/Title';
+import { Input } from '../../components/Input';
 import { Textarea } from "../../components/Textarea";
 import { Section } from '../../components/Section';
 import { NoteItem } from "../../components/NoteItem";
+import { Button } from "../../components/Button";
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
 
-import {Container } from './styles'
+import { Container, Form } from './styles'
 
 export function New(){
   return(
     <Container>
-      <div>New Page</div>
+      <Header/>
 
-      <Textarea placeholder='Observações'/>
+      <main>
+        <Form>
+          <Title title='Novo Filme'>
+            <Link to='/'>
+              <button className='back-btn'><FiArrowLeft/>Voltar</button>
+            </Link>
+          </Title>
 
-      <Section title='Marcadores'>
-        <div className='tags'>
-          <NoteItem value='https://rocketseat.com'/>
-          <NoteItem isNew placeholder='Novo Link'/>
-        </div>
-      </Section>
+          <div className='group-input'>
+            <Input placeholder='Título'/>
+            <Input placeholder='Sua nota (de 0 a 5)'/>
+          </div>
+
+          <Textarea placeholder='Observações'/>
+        </Form>
+      </main>
 
 
     </Container>
--- src/pages/New/styles.js
+++ src/pages/New/styles.js
@@ -1,11 +1,48 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 105px auto;
+  grid-template-areas:
+  "header"
+  "content";
+
+  >main {
+    grid-area: content;
+    overflow-y: auto;
+  }
+
   .tags{
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
     background-color: ${({ theme }) => theme.COLORS.BLACK};
     padding: 16px;
+  }
+`
+export const Form = styled.form`
+  width: 80%;
+  margin: 38px auto;
+
+   >div:nth-child(1){
+    flex-direction: column-reverse;
+    align-items: flex-start;
+    margin: 40px 0;
+    height: 92px;
+  }
+
+  .back-btn{
+    background: none;
+    border: none;
+    color:  ${({ theme }) => theme.COLORS.PINK};
+  }
+
+  .group-input{
+    display: flex;
+    justify-content: space-between;
+    gap: 40px;
   }
 `
