d99420de27f88fd334a15da3ff41810230f4548c
pedro558 <pedroafonso109@gmail.com>
2023-07-14 10:51:01 -0300

    :sparkles: feat: add rest param

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,8 +1,8 @@
 import { Container } from './styles'
 
-export function Title({ title, children }){
+export function Title({ title, children, ...rest }){
   return(
-    <Container>
+    <Container {...rest}>
       <h2>{title}</h2>
       {children}
     </Container>
