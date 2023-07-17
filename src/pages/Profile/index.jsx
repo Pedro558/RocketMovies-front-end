5598bd75a946574575b6d19436f9240cf6799c0f
pedro558 <pedroafonso109@gmail.com>
2023-07-17 10:59:15 -0300

    :recycle: refactor: pass backBtn to a component

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,19 +1,16 @@
-import { Link } from 'react-router-dom'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { BackButton } from '../../components/BackButton'
 
 import { Container, Form, Avatar } from './styles'
 
-import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
+import { FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi'
 
 export function Profile(){
   return(
     <Container>
       <header>
-        <Link to='/'>
-          <FiArrowLeft />
-          <span>Voltar</span>
-        </Link>
+        <BackButton title='Voltar'/>
       </header>
 
       <Form>
