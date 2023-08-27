1f02edb58c8dd89686df897cd390e991d444441e
pedro558 <pedroafonso109@gmail.com>
2023-08-27 07:50:08 -0300

    :lipstick: style: Select component

---
+++ src/components/Select/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 8px;
+  
+  > select{
+    height: 76px;
+    padding: 1.4rem;
+
+    border: none;
+    border-radius: 10px;
+
+    color: ${({ theme }) => theme.COLORS.GRAY_300};
+
+    appearance: none;
+    background-image: url("/src/assets/seta.svg");
+    background-repeat: no-repeat;
+    background-position: right 2rem top 50%;
+    background-size: 1.6rem auto;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+  }
+`
